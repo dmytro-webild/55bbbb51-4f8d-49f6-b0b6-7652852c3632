a947a62885d0ceb0b90b3e00055d1d5a4b791762
bender <kudin.dima@gmail.com>
2026-05-17 17:30:27 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function LandingPage() {
       <MediaAbout
       useInvertedBackground={false}
       title="About Our Team"
-      description="Luun nace de la pasión por los detalles y la excelencia. Fusionamos ingredientes locales de alta gama con técnicas artesanales en un entorno diseñado para la conexión y la creatividad."
+      description="Hi, we create simple and professional websites for small businesses in the USA.                                                        My goal is to help local businesses get more clients with a clean online presence."
       imageSrc="https://img.b2bpic.net/free-vector/blue-technology-digital-landscape-futuristic-background_1017-24919.jpg?id=7887838&_wi=6"
       imageAlt="Interior de Luun Brunch & Coffee"
     />
